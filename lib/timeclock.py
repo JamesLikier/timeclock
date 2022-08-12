3125a9e680a265091e739b464fe6e0ca51520574
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-08-11 20:53:47 -0500

    added base class definitions

---
+++ lib/timeclock.py
@@ -0,0 +1,17 @@
+class TimeClock:
+    pass
+
+class EmployeeController:
+    pass
+
+class PunchController:
+    pass
+
+class AuthController:
+    pass
+
+class Punch:
+    pass
+
+class Employee:
+    pass
