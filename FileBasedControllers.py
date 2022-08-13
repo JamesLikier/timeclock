b9b3d2648fb7031976c110640b4d6d6ba16e1d27
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-08-12 20:54:30 -0500

    added skeleton methods to file based controllers

--- FileBasedControllers.py
+++ FileBasedControllers.py
@@ -1,11 +1,47 @@
 import lib.timeclock
 
 class FileBasedEmployeeController(timeclock.EmployeeController):
-    pass
+    def __init__(self, filename: string):
+        super().__init__(self)
+        self.filename = filename
 
+    def createEmployee(self, fname: string, lname: string, admin: bool) -> Employee:
+        pass
+
+    def getEmployeeById(self, employeeId: int) -> Employee:
+        pass
+
+    def modifyEmployee(self, employee: Employee) -> Employee:
+        pass
+    
 class FileBasedPunchController(timeclock.PunchController):
-    pass
+    def __init__(self, filename: string):
+        super().__init__(self)
+        self.filename = filename
+
+    def createPunch(self,
+                    employeeId: int,
+                    datetime: time.struct_time,
+                    createdByEmployeeId: int) -> Punch:
+        pass
+
+    def getPunchById(self, punchId: int) -> Punch:
+        pass
+
+    def getPunchesByEmployeeId(self,
+                            employeeId: int,
+                            startDatetime: time.struct_time,
+                            endDatetime: time.struct_time) -> List[Punch]:
+        pass
+
+    def modifyPunch(self, punch: Punch) -> Punch:
+        pass
+
+    def getPunchCountUpToPunchId(self, punchId: int) -> int:
+        pass
 
 class FileBasedAuthController(timeclock.AuthController):
-    pass
+    def __init__(self, filename: string):
+        super().__init__(self)
+        self.filename = filename
  
