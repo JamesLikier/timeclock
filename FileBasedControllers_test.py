1e41e37e2296ae3355fcbcde8782afacd7857ec3
James Likier <james.likier@gmail.com>
2022-08-13 15:13:24 -0500

    added skeleton of testing for FileBased controllers

---
+++ FileBasedControllers_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+class TestFileBasedEmployeeController(unittest.TestCase):
+    def test_createEmployee(self):
+        pass
+
+    def test_getEmployeeById(self):
+        pass
+
+    def test_modifyEmployee(self):
+        pass
+
+class TestFileBasedPunchController(unittest.TestCase):
+    def test_createPunch(self):
+        pass
+
+    def test_getPunchById(self):
+        pass
+    
+    def test_getPunchesByEmployeeId(self):
+        pass
+    
+    def test_modifyPunch(self):
+        pass
+
+    def test_getPunchCountUpToPunch(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
