b6c19f65991d076356af315432e8728a0ce72194
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-08-12 20:46:16 -0500

    Added file based controllers

---
+++ FileBasedControllers.py
@@ -0,0 +1,11 @@
+import lib.timeclock
+
+class FileBasedEmployeeController(timeclock.EmployeeController):
+    pass
+
+class FileBasedPunchController(timeclock.PunchController):
+    pass
+
+class FileBasedAuthController(timeclock.AuthController):
+    pass
+ 
