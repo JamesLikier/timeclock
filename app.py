b745a592ba74adec154a579f33920f09dfce01ea
James Likier <james.likier@gmail.com>
2022-08-16 20:56:46 -0500

    cleaning up to begin fleshing out routes

--- app.py
+++ app.py
@@ -14,24 +14,14 @@ def routeStatic(req: httprequest, match: Match, sock: socket):
     with open(f"./{req.uri}", 'rb') as f:
         resp = httpresponse(statuscodes.OK)
         resp.body = f.read()
-        sock.send(resp.format())
+        resp.send(sock)
 
 @server.register(("GET",),"/$")
 def routeRoot(req: httprequest, match: Match, sock: socket):
     with open('index.html','rb') as f:
         resp = httpresponse(statuscodes.OK)
         resp.body = f.read()
-        sock.send(resp.format())
-
-@server.register(("GET",),"/json/punch/id/([0-9]+)$")
-def routeGetJSON_punchById(req: httprequest, match: Match, sock: socket):
-    resp = httpresponse(statuscodes.OK)
-    resp.body = match.group(1).encode()
-    sock.send(resp.format())
-
-@server.register(("GET",),"/json/employee/id/([0-9])+$")
-def routeGetJSON_employeeById(req: httprequest, match: Match, sock: socket):
-    pass
+        resp.send(sock)
 
 server.start()
 while(True):
