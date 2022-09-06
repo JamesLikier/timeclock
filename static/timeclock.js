db7593cbe7f23c01410b7b80011c38d3780d1098
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-09-06 17:44:42 -0500

    Added collapse functionality.

--- static/timeclock.js
+++ static/timeclock.js
@@ -1,8 +1,50 @@
-document.addEventListener("click", (event) => {
-    if (event.target.classList.contains("collapse-toggle")) {
-        parent = event.target.parentElement;
-        collapse = parent.querySelector(".collapse");
-        parent.classList.toggle("flex-row");
-        collapse.classList.toggle("d-flex");
+function collapsed(e) {
+    if (e.getAttribute("collapse") === "hide") {
+        e.target.classList.add("d-none");
+    } else {
+        e.target.style.height = null;
+    }
+    e.target.classList.remove("collapsing");
+    e.target.removeEventListener("transitionend", collapsed);
+}
+
+function collapseHide(e) {
+    window.requestAnimationFrame(timestamp => {
+        e.style.height = `${e.scrollHeight}px`;
+        window.requestAnimationFrame(timestamp => {
+            e.classList.toggle("hide");
+        });
+    });
+    e.setAttribute("collapse", "hide");
+}
+
+function collapseShow(e) {
+    window.requestAnimationFrame(t => {
+        e.classList.remove("d-none");
+        window.requestAnimationFrame(t => {
+            e.classList.remove("hide");
+        });
+    });
+    e.setAttribute("collapse", "show");
+}
+
+function toggleCollapse(e) {
+    if (e.getAttribute("collapse") === "hide") {
+        collapseShow(e);
+    } else {
+        collapseHide(e);
+    }
+    e.addEventListener("transitionend", collapsed);
+    e.classList.add("collapsing");
+}
+
+document.addEventListener("click", event => {
+    let collapseTarget = event.target.getAttribute("collapse-target");
+    if (collapseTarget != null) {
+        toggleCollapse(document.querySelector(`#${collapseTarget}`));
+    } else if (event.target.classList.contains("collapse-toggle")) {
+        let parent = event.target.parentElement;
+        let collapseElement = parent.querySelector(".collapse");
+        toggleCollapse(collapseElement);
     }
 });
