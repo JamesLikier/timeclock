f718749528975eab5b198351d405aa15714fbef0
James Likier <james.likier@gmail.com>
2022-09-07 13:49:43 -0500

    Changed collapse width. Added css classes for transitions. Changed collapse functionality:  I originally designed it in a jsfiddle while away from my home computer.  After adding it to my codebase, I had to make some changes.  I also did not originally design it to handle window size changes, so I added functionality to handle this, while still allowing for visibility if javasript is disabled for whatever reason.  I also originally had collapse with mostly css, but realized that to get the functionality I wanted, I needed to handle window resize with javascript.

--- static/timeclock.js
+++ static/timeclock.js
@@ -1,5 +1,5 @@
 function collapsed(e) {
-    if (e.getAttribute("collapse") === "hide") {
+    if (e.target.getAttribute("collapse") === "hide") {
         e.target.classList.add("d-none");
     } else {
         e.target.style.height = null;
@@ -8,31 +8,48 @@ function collapsed(e) {
     e.target.removeEventListener("transitionend", collapsed);
 }
 
-function collapseHide(e) {
-    window.requestAnimationFrame(timestamp => {
-        e.style.height = `${e.scrollHeight}px`;
+function collapseHide(e,transition=true) {
+    effect = e.getAttribute("transition");
+    if (transition) {
         window.requestAnimationFrame(timestamp => {
-            e.classList.toggle("hide");
+            e.style.height = `${e.scrollHeight}px`;
+            window.requestAnimationFrame(timestamp => {
+                e.classList.add("hide");
+            });
         });
-    });
+    } else {
+        e.style.height = `${e.scrollHeight}px`;
+        e.classList.remove(effect);
+        e.classList.add("d-none");
+        e.classList.add("hide");
+    }
     e.setAttribute("collapse", "hide");
 }
 
-function collapseShow(e) {
-    window.requestAnimationFrame(t => {
-        e.classList.remove("d-none");
+function collapseShow(e,transition=true) {
+    effect = e.getAttribute("transition");
+    if (transition) {
+        e.classList.add(effect);
         window.requestAnimationFrame(t => {
-            e.classList.remove("hide");
+            e.classList.remove("d-none");
+            window.requestAnimationFrame(t => {
+                e.classList.remove("hide");
+            });
         });
-    });
+    } else {
+        e.classList.remove(effect);
+        e.style.height = null;
+        e.classList.remove("hide");
+        e.classList.remove("d-none");
+    }
     e.setAttribute("collapse", "show");
 }
 
 function toggleCollapse(e) {
-    if (e.getAttribute("collapse") === "hide") {
-        collapseShow(e);
-    } else {
+    if (e.getAttribute("collapse") === "show") {
         collapseHide(e);
+    } else {
+        collapseShow(e);
     }
     e.addEventListener("transitionend", collapsed);
     e.classList.add("collapsing");
@@ -47,4 +64,26 @@ document.addEventListener("click", event => {
         let collapseElement = parent.querySelector(".collapse");
         toggleCollapse(collapseElement);
     }
+});
+let lastWidth = -1;
+let collapseWidth = 1000;
+function initializeCollapseState() {
+    let e = document.querySelector(".collapse");
+    //window first created
+    if (lastWidth === -1) {
+        if(window.innerWidth <= collapseWidth) {
+            collapseHide(e,false);
+        } 
+    //window was resized from large to small
+    } else if (window.innerWidth <= collapseWidth && lastWidth > collapseWidth) {
+        collapseHide(e,false);
+    //window was resized from small to large
+    } else if (window.innerWidth > collapseWidth && lastWidth <= collapseWidth) {
+        collapseShow(e,false);
+    }
+    lastWidth = window.innerWidth;
+}
+initializeCollapseState();
+window.addEventListener("resize", event=> {
+    initializeCollapseState();
 });
