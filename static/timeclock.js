b3793ec33804d3d81cae32d59690c3da42718c44
James Likier <james.likier@gmail.com>
2022-09-08 18:40:47 -0500

    Wrapped timeclock.js in anonymous function, to remove from global namespace. Ran reformatting for for new layer of braces.

--- static/timeclock.js
+++ static/timeclock.js
@@ -1,89 +1,91 @@
-function collapsed(e) {
-    if (e.target.getAttribute("collapse") === "hide") {
-        e.target.classList.add("d-none");
-    } else {
-        e.target.style.height = null;
+(function () {
+    function collapsed(e) {
+        if (e.target.getAttribute("collapse") === "hide") {
+            e.target.classList.add("d-none");
+        } else {
+            e.target.style.height = null;
+        }
+        e.target.classList.remove("collapsing");
+        e.target.removeEventListener("transitionend", collapsed);
     }
-    e.target.classList.remove("collapsing");
-    e.target.removeEventListener("transitionend", collapsed);
-}
 
-function collapseHide(e,transition=true) {
-    effect = e.getAttribute("transition");
-    if (transition) {
-        window.requestAnimationFrame(timestamp => {
-            e.style.height = `${e.scrollHeight}px`;
+    function collapseHide(e, transition = true) {
+        effect = e.getAttribute("transition");
+        if (transition) {
             window.requestAnimationFrame(timestamp => {
-                e.classList.add("hide");
+                e.style.height = `${e.scrollHeight}px`;
+                window.requestAnimationFrame(timestamp => {
+                    e.classList.add("hide");
+                });
             });
-        });
-    } else {
-        e.style.height = `${e.scrollHeight}px`;
-        e.classList.remove(effect);
-        e.classList.add("d-none");
-        e.classList.add("hide");
+        } else {
+            e.style.height = `${e.scrollHeight}px`;
+            e.classList.remove(effect);
+            e.classList.add("d-none");
+            e.classList.add("hide");
+        }
+        e.setAttribute("collapse", "hide");
     }
-    e.setAttribute("collapse", "hide");
-}
 
-function collapseShow(e,transition=true) {
-    effect = e.getAttribute("transition");
-    if (transition) {
-        e.classList.add(effect);
-        window.requestAnimationFrame(t => {
-            e.classList.remove("d-none");
+    function collapseShow(e, transition = true) {
+        effect = e.getAttribute("transition");
+        if (transition) {
+            e.classList.add(effect);
             window.requestAnimationFrame(t => {
-                e.classList.remove("hide");
+                e.classList.remove("d-none");
+                window.requestAnimationFrame(t => {
+                    e.classList.remove("hide");
+                });
             });
-        });
-    } else {
-        e.classList.remove(effect);
-        e.style.height = null;
-        e.classList.remove("hide");
-        e.classList.remove("d-none");
+        } else {
+            e.classList.remove(effect);
+            e.style.height = null;
+            e.classList.remove("hide");
+            e.classList.remove("d-none");
+        }
+        e.setAttribute("collapse", "show");
     }
-    e.setAttribute("collapse", "show");
-}
 
-function toggleCollapse(e) {
-    if (e.getAttribute("collapse") === "show") {
-        collapseHide(e);
-    } else {
-        collapseShow(e);
+    function toggleCollapse(e) {
+        if (e.getAttribute("collapse") === "show") {
+            collapseHide(e);
+        } else {
+            collapseShow(e);
+        }
+        e.addEventListener("transitionend", collapsed);
+        e.classList.add("collapsing");
     }
-    e.addEventListener("transitionend", collapsed);
-    e.classList.add("collapsing");
-}
 
-document.addEventListener("click", event => {
-    let collapseTarget = event.target.getAttribute("collapse-target");
-    if (collapseTarget != null) {
-        toggleCollapse(document.querySelector(`#${collapseTarget}`));
-    } else if (event.target.classList.contains("collapse-toggle")) {
-        let parent = event.target.parentElement;
-        let collapseElement = parent.querySelector(".collapse");
-        toggleCollapse(collapseElement);
+    document.addEventListener("click", event => {
+        let collapseTarget = event.target.getAttribute("collapse-target");
+        if (collapseTarget != null) {
+            toggleCollapse(document.querySelector(`#${collapseTarget}`));
+        } else if (event.target.classList.contains("collapse-toggle")) {
+            let parent = event.target.parentElement;
+            let collapseElement = parent.querySelector(".collapse");
+            toggleCollapse(collapseElement);
+        }
+    });
+    let lastWidth = -1;
+    let collapseWidth = 800;
+    function initializeCollapseState() {
+        let e = document.querySelector(".collapse");
+        //window first created
+        if (lastWidth === -1) {
+            if (window.innerWidth <= collapseWidth) {
+                collapseHide(e, false);
+            }
+            //window was resized from large to small
+        } else if (window.innerWidth <= collapseWidth && lastWidth > collapseWidth) {
+            collapseHide(e, false);
+            //window was resized from small to large
+        } else if (window.innerWidth > collapseWidth && lastWidth <= collapseWidth) {
+            collapseShow(e, false);
+        }
+        lastWidth = window.innerWidth;
     }
-});
-let lastWidth = -1;
-let collapseWidth = 800;
-function initializeCollapseState() {
-    let e = document.querySelector(".collapse");
-    //window first created
-    if (lastWidth === -1) {
-        if(window.innerWidth <= collapseWidth) {
-            collapseHide(e,false);
-        } 
-    //window was resized from large to small
-    } else if (window.innerWidth <= collapseWidth && lastWidth > collapseWidth) {
-        collapseHide(e,false);
-    //window was resized from small to large
-    } else if (window.innerWidth > collapseWidth && lastWidth <= collapseWidth) {
-        collapseShow(e,false);
-    }
-    lastWidth = window.innerWidth;
-}
-initializeCollapseState();
-window.addEventListener("resize", event=> {
     initializeCollapseState();
-});
+    window.addEventListener("resize", event => {
+        initializeCollapseState();
+    });
+})();
