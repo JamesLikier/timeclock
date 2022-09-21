ce5e8bb7f15aae63bfec4336a8918405d05c086a
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-09-20 21:00:35 -0500

    Coming up with initial basic design for AuthHandler

--- lib/auth.py
+++ lib/auth.py
@@ -1,3 +1,15 @@
 class AuthHandler():
     def __init__(self):
         pass
+    
+    ##returns valid login info: bool
+    def validateCredentials(username: str, password: str) -> bool:
+        pass
+
+    ##returns sessionid: str
+    def createSession(userid: int) -> str:
+        pass
+
+    ##returns valid session: bool
+    def validateSession(userid: int, sessionid: str) -> bool:
+        pass
