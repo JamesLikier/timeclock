03f0fc14c71493418e3ef3f9c71eeb4b1ae3c1af
James Likier <james.likier@gmail.com>
2022-09-21 13:17:09 -0500

    AuthHandler: Added basic functionality to allow for simple use in webapp.

--- lib/auth.py
+++ lib/auth.py
@@ -1,15 +1,28 @@
+import os
+
 class AuthHandler():
     def __init__(self):
-        pass
+        self.sessions = dict()
+        self.sessionByteSize = 32
     
     ##returns valid login info: bool
-    def validateCredentials(username: str, password: str) -> bool:
-        pass
+    def validateCredentials(self, username: str, password: str) -> bool:
+        return True
 
     ##returns sessionid: str
-    def createSession(userid: int) -> str:
-        pass
+    def createSession(self, userid: int) -> str:
+        sid = os.urandom(self.sessionByteSize).hex()
+        self.sessions[userid] = sid
+        return sid
 
     ##returns valid session: bool
-    def validateSession(userid: int, sessionid: str) -> bool:
-        pass
+    def validateSession(self, userid: int, sessionid: str) -> bool:
+        return len(self.sessions.get(userid, '')) > 0
+
+
+if __name__ == '__main__':
+    ah = AuthHandler()
+    sid = ah.createSession(1)
+    print(sid)
+    print(ah.validateSession(1,sid))
+    print(ah.validateSession(2,sid))
