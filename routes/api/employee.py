ddbf2d48b81cbafdc4d45519386ef0213b579428
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-09-26 17:32:40 -0500

    routes/api/employee.py: Started to add routes for api calls for employee functions.
    todo.txt:
    Added todo file for reminding myself of what I'm working on.

---
+++ routes/api/employee.py
@@ -0,0 +1,41 @@
+import re
+import settings
+from httphelper import Request, Response, STATUS_CODES
+from re import Match
+from socket import socket
+import json
+
+session = settings.SESSION_HANDLER
+rh = settings.ROUTE_HANDLER
+jinja = settings.JINJA
+ec = settings.EMPLOYEE_CONTROLLER
+
+@rh.register(["POST"],"/api/employee/new")
+def employeeNew(req: Request, match: Match, sock: socket):
+    valid, userid = session.validateSession(req)
+    resp = Response()
+    data = dict()
+    if valid:
+        try:
+            args = {
+                "fname": req.form["fname"].asStr(),
+                "lname": req.form["lname"].asStr(),
+                "admin": req.form["admin"].asBool()
+            }
+            e = ec.creatEmployee(**args)
+            data["result"] = "success"
+            data["employeeid"] = e.id
+        except Exception:
+            data["result"] = "fail"
+    else:
+        data["result"] = "fail"
+    resp.body = json.dumps(data)
+    resp.send(sock)
+
+@rh.register(["POST"],"/api/employee/edit")
+def employeeEdit(req: Request, match: Match, sock: socket):
+    pass
+
+@rh.register(["POST"], "/api/employee/delete")
+def employeeDelete(req: Request, match: Match, sock: socket):
+    pass
