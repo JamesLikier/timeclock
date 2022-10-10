6b6078e8cb366e630ef74d749215cd489c9e784a
James Likier <james.likier@gmail.com>
2022-10-10 04:36:05 -0500

    Removed a debug logging.

--- routes/api/punch.py
+++ routes/api/punch.py
@@ -21,14 +21,12 @@ ec = settings.EMPLOYEE_CONTROLLER
 def punchNew(req: Request, match: Match, sock: socket):
     msg = Message()
     msg.action = "punch/new"
-    logging.debug(f'New Punch Request: {req.form._data=}')
     try:
         isoDate = timeclock.convertDateStrToISO(req.form["date"].asStr()) or dt.date.today().isoformat()
         date = dt.date.fromisoformat(isoDate)
         isoTime = req.form["time"].asStr() or dt.datetime.now().time().isoformat()
         time = dt.time.fromisoformat(isoTime)
         datetime = dt.datetime.combine(date,time)
-        logging.debug(f'Datetime: {datetime}')
         e = ec.getEmployeeById(req.form["employeeid"].asInt())
         pc.createPunch(e.id,datetime)
         msg.result = Message.SUCCESS
