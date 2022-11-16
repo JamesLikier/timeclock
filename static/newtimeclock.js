10f2aeb781c3c2a6e49c93148c3349c738f99025
JamesLikier <110158610+JamesLikier@users.noreply.github.com>
2022-11-16 15:38:35 -0600

    created callbacks.

--- static/newtimeclock.js
+++ static/newtimeclock.js
@@ -1,45 +1,26 @@
 const tc = {};
 (function (tc) {
-    tc.SUCCESS = "success";
-    tc.FAIL = "fail";
-    tc.accessToken = null;
+    const callbacks = {};
+    function registerCallback(key,fn) {
+        callbacks[key] = fn;
+    }
     function apiCall(url, data = {}, callback) {
-        data.accessToken = tc.accessToken;
         fetch(url, data)
         .then(r => r.json())
         .then(r => callback(r));
     }
+    document.addEventListener('submit', e => {
+        const form = e.target;
+        const fd = new FormData(form);
+        apiCall(form.action, {body: fd}, callbacks[form.dataset.callback]);
+    });
 
     // Display Layer Stack
-    
     // End Display Layer Stack
 
     // User Auth Functions
-    tc.login = function (username, password) {
-        apiCall(
-            "/api/login",
-            data = {
-                method: "POST",
-                body: JSON.stringify({ username: username, password: password })
-            },
-            r => {
-                if (r.result == tc.SUCCESS) {
-                } else {
-                }
-            }
-        );
-    }
-    tc.logout = function () {
-        apiCall(
-            "/api/logout",
-            data = {
-                method: "POST"
-            }
-        );
-    }
     // End User Auth Functions
 
     // Views
-
     // End Views
 })(tc);
